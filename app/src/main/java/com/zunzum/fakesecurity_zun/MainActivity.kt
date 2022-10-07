4fd8c284b9fa80f386d3af0c1f159834a15775db
zunzum <zunzum@naver.com>
2022-10-07 20:21:51 +0900

    이미지 회전하고싶은데 회전이 안됨 정신이 나갈거같다
    문제점 1. 메인화면에서 저 줄이 왜생기는지 모르겠음
    문제점 2. 밑에 버특 색 왜저래
    문제점 3. 도대체 이미지 회전시키는거 어케하는지 모르겠음

--- app/src/main/java/com/zunzum/fakesecurity_zun/MainActivity.kt
+++ app/src/main/java/com/zunzum/fakesecurity_zun/MainActivity.kt
@@ -2,10 +2,14 @@ package com.zunzum.fakesecurity_zun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val animation = AnimationUtils.loadAnimation(this, R.anim.rotation)
+        dolgi.startAnimation(animation)
     }
 }
